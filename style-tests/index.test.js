bf50ab721061ac548a8a8d5f960a5d95a7237aef
Olga Kazakova <olegenok@bk.ru>
2022-06-27 18:11:49 +0300

    delete run screenshort test sanitycheck

--- style-tests/index.test.js
+++ style-tests/index.test.js
@@ -4,41 +4,36 @@ import { toMatchImageSnapshot } from "jest-image-snapshot";
 expect.extend({ toMatchImageSnapshot });
 
 describe("styles.test", () => {
-    // Give time to any async operation to complete after each test
-
-    let originalTimeout;
-    beforeEach(() => {
+  let originalTimeout;
+  beforeEach(() => {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
-    afterEach(() => (jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout));   
+  });
+  afterEach(() => (jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout));
 
-    [
-        { width: 529, height: 2000 },
-        { width: 605, height: 1500 },
-        { width: 900, height: 1500 },
-    ].forEach(({ width, height }) =>
-        it(`should have proper view for ${width}x${height} params`, async () => {
-            // setting up puppeteer
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            // set current view port size
-            await page.setViewport({ width, height });
-            // navigate to the page, served with webpack
-            // IMPORTANT!: test assumes webpack is started
-            await page.goto("http://localhost:9001", { waitUntil: "networkidle0" });
+  [
+    { width: 529, height: 2000 },
+    { width: 605, height: 1500 },
+    { width: 900, height: 1500 },
+  ].forEach(({ width, height }) =>
+    it(`should have proper view for ${width}x${height} params`, async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.setViewport({ width, height });
 
-            const image = await page.screenshot();
-            await browser.close();
+      await page.goto("http://localhost:9001", { waitUntil: "networkidle0" });
 
-            expect(image).toMatchImageSnapshot(
-                process.env.CI
-                    ? {
-                        failureThreshold: 0.005,
-                        failureThresholdType: "percent",
-                    }
-                    : undefined
-            );
-        })
-    );
+      const image = await page.screenshot();
+      await browser.close();
+
+      expect(image).toMatchImageSnapshot(
+        process.env.CI
+          ? {
+              failureThreshold: 0.005,
+              failureThresholdType: "percent",
+            }
+          : undefined
+      );
+    })
+  );
 });
